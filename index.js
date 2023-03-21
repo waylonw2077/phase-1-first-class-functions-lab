d28b39fb7fd13869d80574a61d1c870c7f26a51a
waylon wickline <waylonw2077@LAPTOP-6MQVH55Q>
2023-03-21 16:56:44 -0400

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+const returnFirstTwoDrivers = function () {
+   return drivers.slice(0,2)
+}
+const returnLastTwoDrivers = function () {
+    return drivers.slice(-2,)
+ }
+const selectingDrivers = [returnFirstTwoDrivers , returnLastTwoDrivers]
+function createFareMultiplier(integer) {
+    return function(fare) {
+        return fare * integer;
+      } 
+}
+const Double = createFareMultiplier(integer);
+function fareDoubler(fare) {
+    return fare * 2;
+}
+const triple = createFareMultiplier(integer);
+function fareTripler(fare) {
+    return fare * 3;
+}
+function selectDifferentDrivers(drivers, driverSelector) {
+    return driverSelector(drivers)
+}
+selectDifferentDrivers(drivers, returnFirstTwoDrivers);
